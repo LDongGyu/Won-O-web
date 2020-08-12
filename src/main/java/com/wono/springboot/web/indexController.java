817bdea535fbc0df2b017e80d7016d093c873c9e
LDongGyu <ehdrbchd@naver.com>
2020-08-12 21:16:34 +0900

    feat: indexController에 create, update 링크 추가

--- src/main/java/com/wono/springboot/web/indexController.java
+++ src/main/java/com/wono/springboot/web/indexController.java
@@ -1,10 +1,12 @@
 package com.wono.springboot.web;
 
 import com.wono.springboot.service.posts.PostsService;
+import com.wono.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,5 +17,15 @@ public class indexController {
     @GetMapping("/")
     public String index(Model model){
         return "index";
+    }
+
+    @GetMapping("/posts/create")
+    public String create(){
+        return "posts-create";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String update(@PathVariable Long id, Model model){
+        return "posts-update";
     }
 }
