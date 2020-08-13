717060b2ed4fba02c87c9e8217478c3bc50755fa
LDongGyu <ehdrbchd@naver.com>
2020-08-13 20:14:21 +0900

    feat: PostsService에 findById 추가

--- src/main/java/com/wono/springboot/service/posts/PostsService.java
+++ src/main/java/com/wono/springboot/service/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.wono.springboot.service.posts;
 
+import com.wono.springboot.domain.posts.Posts;
 import com.wono.springboot.domain.posts.PostsRepository;
+import com.wono.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
 }
