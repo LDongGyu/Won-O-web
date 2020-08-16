910f289c7269e6a54263c2073ca36effea514cb8
LDongGyu <ehdrbchd@naver.com>
2020-08-16 19:59:30 +0900

    feat: PostsService에 update 추가

--- src/main/java/com/wono/springboot/service/posts/PostsService.java
+++ src/main/java/com/wono/springboot/service/posts/PostsService.java
@@ -4,6 +4,7 @@ import com.wono.springboot.domain.posts.Posts;
 import com.wono.springboot.domain.posts.PostsRepository;
 import com.wono.springboot.web.dto.PostsCreateRequestDto;
 import com.wono.springboot.web.dto.PostsResponseDto;
+import com.wono.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,8 +19,18 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto postsUpdateRequestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(posts.getTitle(), posts.getContent());
+
+        return id;
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
 }
