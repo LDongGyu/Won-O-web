1502e0abba967d8c3b347be99b80109b7b3b4c58
LDongGyu <ehdrbchd@naver.com>
2020-08-16 20:00:06 +0900

    feat: postsApiController에 update api 추가

--- src/main/java/com/wono/springboot/web/postsApiController.java
+++ src/main/java/com/wono/springboot/web/postsApiController.java
@@ -3,6 +3,7 @@ package com.wono.springboot.web;
 
 import com.wono.springboot.service.posts.PostsService;
 import com.wono.springboot.web.dto.PostsCreateRequestDto;
+import com.wono.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +16,10 @@ public class postsApiController {
     @PostMapping("/api/v1/posts")
     public Long create(@RequestBody PostsCreateRequestDto postsCreateRequestDto){
         return postsService.create(postsCreateRequestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto postsUpdateRequestDto){
+        return postsService.update(id, postsUpdateRequestDto);
     }
 }
