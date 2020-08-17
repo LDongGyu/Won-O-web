1e312dcde1788320f72561b47508e29759962fee
LDongGyu <ehdrbchd@naver.com>
2020-08-17 15:26:38 +0900

    feat: PostsListResponseDto 추가

---
+++ src/main/java/com/wono/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.wono.springboot.web.dto;
+
+import com.wono.springboot.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title =entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
