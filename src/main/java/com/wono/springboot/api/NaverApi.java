af904972cdfe3fb037b2030b5535d39a54b359e1
LDongGyu <ehdrbchd@naver.com>
2020-08-19 21:20:55 +0900

    feat: NaverApi ArrayList형으로 변경

--- src/main/java/com/wono/springboot/api/NaverApi.java
+++ src/main/java/com/wono/springboot/api/NaverApi.java
@@ -1,35 +1,55 @@
 package com.wono.springboot.api;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.wono.springboot.web.dto.NaverSearchResultDto;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class NaverApi {
 
-    public static String naverApiResult(){
+    public static ArrayList<NaverSearchResultDto> naverApiResult(){
         String clientId = "Jpy_ByAtqu1wNVKXoAOM"; //애플리케이션 클라이언트 아이디값"
         String clientSecret = "sY22L6RLax"; //애플리케이션 클라이언트 시크릿값"
 
         String text = null;
         try {
-            text = URLEncoder.encode("한동숙", "UTF-8");
+            text = URLEncoder.encode("둔치", "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException("검색어 인코딩 실패",e);
         }
 
         String apiURL = "https://openapi.naver.com/v1/search/cafearticle?query=" + text +"&sort=sim&display=100";    // json 결과
 
-
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
         String responseBody = get(apiURL,requestHeaders);
 
-        return responseBody;
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject)jsonParser.parse(responseBody);
+        JsonArray jsonArray = (JsonArray)jsonObject.get("items");
+
+        ArrayList<NaverSearchResultDto> resultList = new ArrayList<>();
+        for(JsonElement json : jsonArray){
+            JsonObject object = (JsonObject) json;
+            String title = object.get("title").toString();
+            String link = object.get("link").toString();
+            String description = object.get("description").toString();
+            NaverSearchResultDto element = new NaverSearchResultDto(title,link,description);
+            resultList.add(element);
+        }
+        return resultList;
     }
 
 
